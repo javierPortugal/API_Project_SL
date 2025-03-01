cb5486d29fc89a69cce20eea94f2e1f2963be1ed
Javier Portugal <javier.s.portugal@gmail.com>
2025-03-01 13:27:31 -0600

    modificactions to test from postmant
    data for testing ion postmant

--- stock-market-backend/index.js
+++ stock-market-backend/index.js
@@ -15,15 +15,21 @@ app.use(express.json());
 let stocks=[
     {id:1, name:'apple', symbol: 'AAPL', price: 175},
     {id:2, name:'tesla', symbol: 'TSLA', price: 175},
-    {id:1, name:'Google', symbol: 'GOGL', price: 175},
-    {id:1, name:'Amazon', symbol: 'AMZN', price: 175},
+    {id:3, name:'Google', symbol: 'GOGL', price: 175},
+    {id:4, name:'Amazon', symbol: 'AMZN', price: 175},
 
 ];
 
 app.get('/stocks', (req, res)=>{
     res.json(stocks);
-
 });
+
+app.get('/stocks/:id', (req, res)=>{
+    const stock=stocks.find(s=>s.id ===parseInt(req.params.id));
+    if (!stock) return res.status(404).json({message:'Stock Not Found!!'});
+    res.send(stock);
+});
+
 
 app.listen(PORT,()=>console.log(`Server is running and Up!! on PORT: ${PORT}`));
 
